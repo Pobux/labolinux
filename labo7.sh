a33aba9844e91022e5c56c5763a95a8774c10bd5
Antoine <1@2>
2018-10-24 16:06:58 -0400

    labo7 init

---
+++ labo7.sh
@@ -0,0 +1,20 @@
+ls -l /usr/lib | sort -k 5 -n | tail -n 1 | cut -d ' ' -f 7 | numfmt --to=si
+
+cat /dev/zero | head -c 1234567 > fichier
+chmod 421 fichier
+ln fichier liendur1
+ln fichier liendur2
+
+gzip toto -f
+
+mv l\'\* "./-|-"
+
+Dans d2
+ln -s ../f0 ../l0
+ln -s f1 ../l1
+ln -s ln -s d2/f2 ../l2
+
+cat /usr/share/dict/french | grep -e "^[^eéèêë]*è\([^eéèêë]*\)$"
+
+cat /usr/share/common-licenses/GPL | grep -ve '^$' | grep -vi e
+
