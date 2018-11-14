e3e694e87e4ae90f39a576c3270188c241980aec
Antoine <1@2>
2018-11-14 16:04:00 -0500

    labo9

---
+++ labo9.sh
@@ -0,0 +1,6 @@
+(xeyes &) && gnome-calculator &
+#CTRL-Z pour intérrompre, fg pour remettre en "foreground"
+#ps -f (pour c'est propre processus)
+#ps -ef | grep [process], pour trouver un processus en particulier
+#ps --forest pour visualiser les processus de manière hiérarchique
+#Pour avoir la liste des signaux, man 7 signal
